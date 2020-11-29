e66ae195d1c85f9c23bba12b04f23c8a2a47bafd
Frank Tran <frank.tran@live.ca>
2020-11-29 11:57:42 -0800

    gradepredicts

---
+++ main.py
@@ -0,0 +1,45 @@
+import tensorflow 
+import keras
+import pandas as pd
+import numpy as np
+import sklearn
+import sklearn.model_selection
+from sklearn import linear_model
+from sklearn.utils import shuffle
+import matplotlib.pyplot as pyplot
+import pickle
+from matplotlib import style
+
+data = pd.read_csv("student-mat.csv", sep=";")
+data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
+
+# Label
+predict = "G3"
+
+#Define our attributes
+X = np.array(data.drop([predict], 1))
+Y = np.array(data[predict])
+
+# Split 10% of our data for testing 
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.1)
+linear = linear_model.LinearRegression()
+#creates best fit line
+linear.fit(x_train, y_train)
+#Calculate accuracy of our model
+acc = linear.score(x_test, y_test)
+print(acc)
+
+with open("studentmodel.pickle", "wb") as f:
+    pickle.dump(linear, f)
+
+pickle_in = open("studentmodel.pickle", "rb")
+linear = pickle.load(pickle_in)
+
+print("Co: \n", linear.coef_)
+print("Intercept: \n", linear.intercept_)
+
+predictions = linear.predict(x_test)
+
+for x in range(len(predictions)):
+    print(predictions[x], x_test[x], y_test[x])
+
